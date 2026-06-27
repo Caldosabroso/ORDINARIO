9eda348761a0c2372030b9d7ee06483eece83349
Caldosabroso <20160863@itoaxaca.edu.mx>
2026-06-27 15:47:14 -0600

    Create Ordinario.c

---
+++ Ordinario.c
@@ -0,0 +1,26 @@
+
+#include "Struct.h"
+#include <stdio.h>
+#include "menu.h"
+
+int main(){
+    int opcion;
+    struct Persona *ptr=NULL;
+    do{
+        opcion=menuPrincipal();
+    
+        switch(opcion){
+            case 1:
+        
+            break;
+            case 2:
+
+        
+            break;
+        }
+    }while(opcion!=5);
+
+
+
+return 0;
+}
