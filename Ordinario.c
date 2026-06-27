39354782db632f0d6b70fecbebbe77cebb87fe39
Caldosabroso <20160863@itoaxaca.edu.mx>
2026-06-27 17:36:56 -0600

    Update Ordinario.c

--- Ordinario.c
+++ Ordinario.c
@@ -1,8 +1,7 @@
-
 #include "Struct.h"
 #include <stdio.h>
 #include "menu.h"
-
+#include "Altas.h"
 int main(){
     int opcion;
     struct Persona *ptr=NULL;
@@ -10,13 +9,17 @@ int main(){
         opcion=menuPrincipal();
     
         switch(opcion){
-            case 1:
+            case 1: altas(&ptr);
         
             break;
             case 2:
 
-        
             break;
+            case 3:
+            break;
+            case 4:
+            break;
+
         }
     }while(opcion!=5);
 
