c072939decf6d0a5b1115f7105a03a9fddc7fcfd
Caldosabroso <20160863@itoaxaca.edu.mx>
2026-06-29 23:35:36 -0600

    Add Bajas functionality and cleanup main loop

--- Ordinario.c
+++ Ordinario.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "menu.h"
 #include "Altas.h"
+#include "Bajas.h"
 int main(){
     int opcion;
     struct Persona *ptr=NULL;
@@ -12,15 +13,20 @@ int main(){
             case 1: altas(&ptr);
         
             break;
-            case 2:
+            case 2: bajas(&ptr);
 
             break;
             case 3:
             break;
             case 4:
             break;
+            case 5: eliminarTodasLasPersonas(&ptr);
+            printf("PROGRAMA FINALIZADO\n");
+            break;
 
-        }
+            default:
+            printf("Opción no válida.\n");
+    }
     }while(opcion!=5);
 
 
